345a0012199b33f7e61f77ac8b770cf1f8a238bc
Samuel Banya <sbanya@fastmail.com>
2023-08-31 19:51:15 -0500

    Adding Styled JSX components to project

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function NavBar() {
   return (
-    <nav className="NavBar">
+    <nav>
       <ul>
         <li>
           <Link href="/">
@@ -15,6 +15,20 @@ export default function NavBar() {
           </Link>
         </li>
       </ul>
+      <style jsx>{`
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline;
+
+}
+
+li:not(:first-child) {
+  margin-left: 0.75rem;
+}
+        `}</style>
     </nav>
   )
 }
