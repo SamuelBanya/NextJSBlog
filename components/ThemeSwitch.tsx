81ca298570386bc47920b0f1a32daf7b1e1ee282
Samuel Banya <sbanya@fastmail.com>
2023-09-07 20:08:56 -0500

    Adding related useState function and loadDarkMode function with local storage for theme

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -1,15 +1,29 @@
 import { useState } from "react";
 import DarkTheme from "./DarkTheme";
 
+function loadDarkMode() {
+  if (typeof localStorage === 'undefined') {
+    return false;
+  }
+  const value = localStorage.getItem('darkMode');
+
+  return (value === null) ? false : JSON.parse(value)
+}
+
 function ThemeSwitch() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(loadDarkMode);
 
   console.log('ThemeSwitch: text: ', darkMode);
   const text = darkMode ? 'Light Mode' : 'Dark Mode';
 
+  const handleClick = () => {
+    localStorage.setItem('darkMode', JSON.stringify(!darkMode));
+    setDarkMode(!darkMode)
+  }
+
   return (
     <>
-      <button onClick={() => setDarkMode(!darkMode)}>{text}</button>
+      <button onClick={handleClick} suppressHydrationWarning>{text}</button>
       <style jsx>{`
 button {
 background: none;
@@ -17,7 +31,7 @@ border: none;
 cursor: pointer;
 color: inherit;
 }
-        `}</style>
+      `}</style>
       {darkMode && <DarkTheme />}
     </>
   )
