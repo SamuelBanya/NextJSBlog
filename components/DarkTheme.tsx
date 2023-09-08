d87eeb7945028e3c58b79c2a8b216fe5e564eae3
Samuel Banya <sbanya@fastmail.com>
2023-09-07 19:50:05 -0500

    Adding Dark mode button

---
+++ components/DarkTheme.tsx
@@ -0,0 +1,14 @@
+function DarkTheme() {
+  return (
+    <style jsx global>{`
+/* Dark Mode: */
+:root {
+  --background-color: black;
+  --text-color: white;
+  --link-color: yellow;
+}
+`}</style>
+  );
+}
+
+export default DarkTheme;
---
+++ components/ThemeSwitch.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import DarkTheme from "./DarkTheme";
+
+function ThemeSwitch() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  console.log('ThemeSwitch: text: ', darkMode);
+  const text = darkMode ? 'Light Mode' : 'Dark Mode';
+
+  return (
+    <>
+      <button onClick={() => setDarkMode(!darkMode)}>{text}</button>
+      <style jsx>{`
+button {
+background: none;
+border: none;
+cursor: pointer;
+color: inherit;
+}
+        `}</style>
+      {darkMode && <DarkTheme />}
+    </>
+  )
+}
+
+export default ThemeSwitch;
